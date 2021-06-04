09172494d45dfa5916c7587c2644282998af0d18
ahmed-asn <67997874+ahmed-asn@users.noreply.github.com>
2021-06-04 14:09:38 +0200

    Update temprature.py

--- temprature.py
+++ temprature.py
@@ -3,27 +3,16 @@ from time import sleep
 from onewire import OneWire
 from ds18x20 import DS18X20
 
-def read_temp(pin):
+def temp(pin):
     ds_pin = Pin(pin)
     sensor = DS18X20(OneWire(ds_pin))
 
     roms = sensor.scan()
-    print("DS Geräte:", roms)
+ 
+    if len(roms) != 0:
+        return sensor.read_temp(roms[0])
+    else:
+        return None
+        
 
-    
-    sensor = False
-    i = 0
-    while not sensor:
-    
-        if len(roms) != 0:
-            return(sensor)
-
-        else :
-            print("Keine Sensoren gefunden! Schaltung kontrollieren, in kürze wird erneut gesucht...")
-            i += 1
-            if i > 2:
-                   print("3 mal Fehlgeschlagen! Versuchen Sies später erneut.")
-                   return(None)
-            
-            
     
